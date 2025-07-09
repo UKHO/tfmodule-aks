36285e7bc6143c818cfe40c2389a9274fc767623
richl-ukho <richard.lang@ukho.gov.uk>
2025-07-09 14:19:36 +0100

    more outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "identity_principal_id" {
 output "secret_identity_principal_id" {
     value = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
 }
+
+output "secret_identity_client_id" {
+    value = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].client_id
+}
+
+output "kubelet_identity_principal_id" {
+    value = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
