6f1838545d003ab915b649072597d62ffcba6f23
richard-shawley-ukho <arichards@ukho.gov.uk>
2025-07-24 13:16:07 +0100

    Added flux extension

--- variables.tf
+++ variables.tf
@@ -185,3 +185,58 @@ variable "ip_rules" {
   type        = list(string)
   default     = []
 }
+
+variable "flux_git_repository_url" {
+  description = "Git repository URL for Flux configuration"
+  type        = string
+  default     = ""
+  
+  validation {
+    condition     = length(var.flux_git_repository_url) > 0
+    error_message = "The flux_git_repository_url variable must be supplied"
+  }
+}
+
+variable "flux_git_reference_type" {
+  description = "Git reference type for Flux configuration (e.g., branch, tag)"
+  type        = string
+  default     = "branch"
+  
+  validation {
+    condition     = length(var.flux_git_reference_type) > 0
+    error_message = "The flux_git_reference_type variable must be supplied"
+  }
+}
+
+variable "flux_git_reference_value" {
+  description = "Git reference value for Flux configuration (e.g., branch name, tag name)"
+  type        = string
+  default     = "main"
+  
+  validation {
+    condition     = length(var.flux_git_reference_value) > 0
+    error_message = "The flux_git_reference_value variable must be supplied"
+  }
+}
+
+variable "flux_ssh_private_key_base64" {
+  description = "Base64 encoded SSH private key for Flux Git repository access"
+  type        = string
+  default     = ""
+  
+  validation {
+    condition     = length(var.flux_ssh_private_key_base64) > 0
+    error_message = "The flux_ssh_private_key_base64 variable must be supplied"
+  }
+}
+
+variable "flux_git_repository_path" {
+  description = "Path to the Flux Git repository configuration"
+  type        = string
+  default     = ""
+  
+  validation {
+    condition     = length(var.flux_git_repository_path) > 0
+    error_message = "The flux_git_repository_path variable must be supplied"
+  }
+}
