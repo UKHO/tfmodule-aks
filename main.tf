0360d39ab8a6593c1823242cc475d969c33fd776
richl-ukho <richard.lang@ukho.gov.uk>
2025-07-29 14:27:24 +0100

    nginx fix

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_kubernetes_cluster" "this" {
 
   web_app_routing {
     dns_zone_ids = []
-    default_ingress_controller_type = "internal"
+    default_nginx_controller = "internal"
   }
 
   identity {
