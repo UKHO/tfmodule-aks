c3dcbe0a85a77c7bf9ee06d8766178dbbee030a9
richl-ukho <richard.lang@ukho.gov.uk>
2025-08-01 12:53:55 +0100

    Use privatelink.uksouth.azmk8s.io

--- data.tf
+++ data.tf
@@ -15,6 +15,6 @@ data "azurerm_subnet" "aks" {
 
 data "azurerm_private_dns_zone" "aks" {
   provider            = azurerm.hub
-  name                = "privatelink.azmk8s.io"
+  name                = "privatelink.uksouth.azmk8s.io"
   resource_group_name = var.dns_resource_group_name
 }
