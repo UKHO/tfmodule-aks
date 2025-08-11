0c8c69066a63ef19766f4220de97c5a7a0af78b4
richl-ukho <richard.lang@ukho.gov.uk>
2025-08-11 14:48:27 +0100

    output webapprouting_identity

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,11 @@ output "secret_identity_client_id" {
 output "kubelet_identity_principal_id" {
     value = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 }
+
+output "webapprouting_identity_client_id" {
+    value = azurerm_kubernetes_cluster.this.web_app_routing_identity[0].client_id
+}
+
+output "webapprouting_identity_principal_id" {
+    value = azurerm_kubernetes_cluster.this.web_app_routing_identity[0].object_id
+}
