1d84703653b05035b57e91d6e0508077e423727b
richl-ukho <richard.lang@ukho.gov.uk>
2026-01-15 08:54:08 +0000

    Add outbound ips

--- outputs.tf
+++ outputs.tf
@@ -33,3 +33,16 @@ output "oidc_issuer_url" {
 output "node_resource_group_id" {
     value = azurerm_kubernetes_cluster.this.node_resource_group_id
 }
+
+output "outbound_ip_addresses" {
+    description = "The outbound public IP addresses used by the AKS cluster"
+    value = [
+        for ip in azurerm_kubernetes_cluster.this.network_profile[0].load_balancer_profile[0].effective_outbound_ips : 
+        ip
+    ]
+}
+
+output "outbound_ip_address" {
+    description = "The primary outbound public IP address used by the AKS cluster"
+    value = length(azurerm_kubernetes_cluster.this.network_profile[0].load_balancer_profile[0].effective_outbound_ips) > 0 ? azurerm_kubernetes_cluster.this.network_profile[0].load_balancer_profile[0].effective_outbound_ips[0] : null
+}
