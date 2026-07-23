756e540c382b8a9c9c30fac9cdeb7486724d6a16
richl-ukho <richard.lang@ukho.gov.uk>
2026-07-23 11:56:06 +0100

    fix: temporarily set system pool max_surge to 0 to work around vCPU quota limit

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_kubernetes_cluster" "this" {
     temporary_name_for_rotation = "tmpsystem"
 
     upgrade_settings {
-      max_surge = "10%"
+      max_surge = "0"
     }
   }
 
